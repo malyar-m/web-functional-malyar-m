6a6f0afe5d7681513ee712cbd239bac1067dcbcf
Michael Maliarchuk <malyar90@gmail.com>
2024-04-05 15:34:01 +0300

    hello (:

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { Controls } from './components/Controls';
 import { CurrentlyReading } from './components/CurrentlyReading';
+import { fetchContent, parseContentIntoSentences } from './lib/content';
 
 function App() {
-  // const [sentences, setSentences] = useState<Array<string>>([]);
+  const [sentences, setSentences] = useState<Array<string>>([]);
   // const { currentWord, currentSentence, controls } = useSpeech(sentences);
+
+  useEffect(() => {
+    // console.log();
+    const sentence = fetchContent().then((data) => {
+      console.log('33', data);
+      
+      const parsed = parseContentIntoSentences(data);
+      setSentences(parsed);
+    });
+    console.log(sentence);
+    
+  }, [])
 
   return (
     <div className="App">
       <h1>Text to speech</h1>
       <div>
-        <CurrentlyReading/>
+        <CurrentlyReading />
       </div>
       <div>
-        <Controls/>
+        <Controls />
       </div>
     </div>
   );
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,15 +1,23 @@
+import { json } from "express";
+
 const API_URL = "http://localhost:5174/content";
 
 /**
  * Fetch the content from the api
  * In case of an error, return content as "<speak><s>There was an error</s></speak>"
  */
-const fetchContent = async (url = API_URL): Promise<string> => {};
+const fetchContent = async (url = API_URL): Promise<string> => {
+    const apiResult = await fetch(API_URL);
+    const jsonResult = await apiResult.json();
+    return jsonResult.content;
+};
 
 /**
  * Parse the content into sentences, and return an array of sentences. Look at the Readme for sample input and expected output.
  * Avoid using DOMParser for implementing this function.
  */
-const parseContentIntoSentences = (content: string) => {};
+const parseContentIntoSentences = (content: string) => {
+    return content.match(/<s>([\w ,.]*)?<\/s>/g);
+};
 
 export { fetchContent, parseContentIntoSentences };
